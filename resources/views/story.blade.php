dda6c3b5b126c0f195f36ec0b7fd0d4463d8d33c
Nnamani Chinaecherem <nnamanijoshua@Nnamanis-MacBook-Pro.local>
2021-05-22 20:51:52 +0100

    view button

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -11,14 +11,16 @@
                 <th scope="col">Title</th>
                 <th scope="col"></th>
                 <th scope="col"></th>
+                <th scope="col"></th>
             </tr>
             </thead>
             @foreach ($posts as $post)
                 <tbody>
                     <tr>
                     <th scope="col">#</th>
-                    <td><a href="/home/{{$post->id}}">{{Str::limit($post->title, 40)}}</a></td>
-                    <td><a href="/edit/{{$post->id}}" class="btn btn-primary btn-sm">Edit</a></td>
+                    <td>{{Str::limit($post->title, 40)}}</td>
+                    <td><a href="/home/{{$post->id}}" class="btn btn-info btn-sm">View</a></td>
+                    <td><a href="/edit/{{$post->id}}" class="btn btn-secondary btn-sm">Edit</a></td>
                     <td><a href="/delete/{{$post->id}}" class="btn btn-danger btn-sm">Delete</a></td>
                     </tr>
             @endforeach  
